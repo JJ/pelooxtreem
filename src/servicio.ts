1226ac0afe1e9824a3e9f0cc9e340e3395e9f70a
Francisco Javier Romero García <fjromeero@correo.ugr.es>
2022-11-18 10:34:16 +0100

    Closes #6 modelo de cada servicio Se ha creado un modelo de cada servicio, para ello se ha usado el modulo https://deno.land/x/durationjs@v4.1.0 para representar la duracion de dicho servicio

---
+++ src/servicio.ts
@@ -0,0 +1,22 @@
+import Duration from "https://deno.land/x/durationjs@v4.0.0/mod.ts"; 
+
+
+export class Servicio{
+
+    // Nombre del servicio
+    nombre: String;
+
+    // Duración medio total que se tarda en realizar servicio
+    tiempoTotal: Duration;
+
+    // Tiempo que se trabaja directamente sobre el cliente
+    tiempoUtil: Duration;
+
+    // Los tiempos han de estar en el siguiente formato: 1h1m2s
+    constructor(nombre:String, tiempoTotal:String, tiempoUtil: String){
+        this.nombre = nombre;
+        this.tiempoTotal = Duration.fromString(tiempoTotal);
+        this.tiempoUtil = Duration.fromString(tiempoUtil);
+    }
+
+}
