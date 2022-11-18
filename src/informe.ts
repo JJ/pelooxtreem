1b25a40ca2225f4fe6f8c3422db15190f6e2b1d3
vdeq79 <xszh79@outlook.com>
2022-11-18 09:54:46 +0100

    Añade el atributo fecha en la clase informe #3

--- src/informe.ts
+++ src/informe.ts
@@ -1,9 +1,12 @@
 import { Turno } from "./turno.ts";
 
 class Informe{
+    fecha: Date;
     turno: Turno;
     nServicio: Map <Servicio, number>;
+    //Tiempo medio de servicios realizados expresados en minutos
     tMedioServicio: Map<Servicio,number>;
     nTrabajador: number;
+    //Tiempo medio de espera expresado en minutos
     tMedioEspera: number;
 }
