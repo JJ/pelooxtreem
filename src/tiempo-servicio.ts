dd203221329a1ad7c5c592fe0783d7cc9e73cb14
PacoCP5 <pacocamaraparra5@gmail.com>
2022-12-02 09:29:34 +0100

    :construction: Elimino atributo nombre de TiempoServicio

--- src/tiempo-servicio.ts
+++ src/tiempo-servicio.ts
@@ -1,6 +1,5 @@
 import Duration from "https://deno.land/x/durationjs@v4.1.0/mod.ts"; 
 
-
 export class TiempoServicio{
 
     // Duración medio total que se tarda en realizar servicio
@@ -10,8 +9,7 @@ export class TiempoServicio{
     tiempoUtil: Duration;
 
     // Los tiempos han de estar en el siguiente formato: 1h1m2s
-    constructor(nombre:string, tiempoTotal:string, tiempoUtil: string){
-        this.nombre = nombre;
+    constructor(tiempoTotal:string, tiempoUtil: string){
         this.tiempoTotal = Duration.fromString(tiempoTotal);
         this.tiempoUtil = Duration.fromString(tiempoUtil);
     }
