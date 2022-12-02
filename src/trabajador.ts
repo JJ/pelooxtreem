141693994de317fe881db5e18d35eb5a44c9ab86
luistf24 <luis.tormo24@gmail.com>
2022-12-02 08:50:14 +0100

    Servicio tiene que pertenecer a Servicio #5
    Lo dejo comentado para que veamos a continuación que hacer con ello

--- src/trabajador.ts
+++ src/trabajador.ts
@@ -1,15 +1,15 @@
-export enum Servicio {
-  corteHombre = "Corte Hombre",
-  corteMujer = "Corte Mujer",
-  mechasHombre = "Mechas Hombre",
-  mechasMujer = "Mechas Mujer",
-  arregloBarba = "Arreglo Barba",
-  lavado = "Lavado",
-  alisado = "Alisado",
-  peinado = "Peinado",
-  ondulacion = "Ondulacion",
-  extensiones = "Extensiones"
-}
+/*export enum Servicio {*/
+  /*corteHombre = "Corte Hombre",*/
+  /*corteMujer = "Corte Mujer",*/
+  /*mechasHombre = "Mechas Hombre",*/
+  /*mechasMujer = "Mechas Mujer",*/
+  /*arregloBarba = "Arreglo Barba",*/
+  /*lavado = "Lavado",*/
+  /*alisado = "Alisado",*/
+  /*peinado = "Peinado",*/
+  /*ondulacion = "Ondulacion",*/
+  /*extensiones = "Extensiones"*/
+/*}*/
 
 
 export class Trabajador{
