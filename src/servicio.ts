666c9893799aef155ae0dd6a020fb908395e4fde
PacoCP5 <pacocamaraparra5@gmail.com>
2022-12-02 09:26:30 +0100

    :construction: Muevo enum tipo_servicio de trabajador.ts a servicio.ts closes #22

--- src/servicio.ts
+++ src/servicio.ts
@@ -1,2 +1,26 @@
+import { TiempoServicio } from "./tiempo-servicio";
+
+export enum tipo_servicio {
+  corteHombre = "Corte Hombre",
+  corteMujer = "Corte Mujer",
+  mechasHombre = "Mechas Hombre",
+  mechasMujer = "Mechas Mujer",
+  arregloBarba = "Arreglo Barba",
+  lavado = "Lavado",
+  alisado = "Alisado",
+  peinado = "Peinado",
+  ondulacion = "Ondulacion",
+  extensiones = "Extensiones"
+}
+    
 export class Servicio {
+
+  nombre: string;
+  tiempo_servicio : TiempoServicio;
+
+  constructor(nombre: string, tiempo_servicio: TiempoServicio){
+    this.nombre = nombre;
+    this.tiempo_servicio = tiempo_servicio;
+  }
+
 }
